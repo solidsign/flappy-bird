b359296fdcdc86b8139136c950fee301172cc0fa
solidsign <49648821+solidsign@users.noreply.github.com>
2021-09-11 23:51:46 +0300

    Changed Player component and created Movable component

---
+++ Assets/Scripts/Components/Movable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public struct Movable
+    {
+        public Rigidbody2D Rigidbody2D;
+        public Transform Transform;
+        public Vector3 Displacement;
+    }
+}
--- Assets/Scripts/Components/Player.cs
+++ Assets/Scripts/Components/Player.cs
@@ -1,11 +1,6 @@
-﻿using UnityEngine;
-
-namespace Components
+﻿namespace Components
 {
     public struct Player
     {
-        public PlayerState PlayerState;
-        public Rigidbody2D Rigidbody2D;
-        public Transform Transform;
     }
 }
