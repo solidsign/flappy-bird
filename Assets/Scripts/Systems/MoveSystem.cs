5449a0def8f6cabb1fab77c4687f8565fcd019de
solidsign <49648821+solidsign@users.noreply.github.com>
2021-09-11 23:52:07 +0300

    Added MoveSystem.cs

---
+++ Assets/Scripts/Systems/MoveSystem.cs
@@ -0,0 +1,21 @@
+﻿using Components;
+using Leopotam.Ecs;
+
+namespace Systems
+{
+    public class MoveSystem : IEcsRunSystem
+    {
+        private EcsFilter<Movable> _filter;
+        public void Run()
+        {
+            foreach (var i in _filter)
+            {
+                var mov = _filter.Get1(i);
+                if (mov.Displacement.magnitude > 0f)
+                {
+                    mov.Rigidbody2D.MovePosition(mov.Transform.position + mov.Displacement);
+                }
+            }
+        }
+    }
+}
