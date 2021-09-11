8ebbbc202243fb5c12bf2ab172c29c3aa98cfaeb
solidsign <49648821+solidsign@users.noreply.github.com>
2021-09-11 23:48:54 +0300

    Added new Configuration properties

---
+++ Assets/Scripts/UnityComponents/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Configuration : ScriptableObject
+{
+    [Header("Jump properties")]
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float jumpTime;
+    [Tooltip("X and Y should be between 0 and 1")]
+    [SerializeField] private AnimationCurve jumpAnimationCurve;
+
+    [Header("Fall properties")]
+    [SerializeField] private float fallSpeed;
+    [Tooltip("Y should be between 0 and 1")]
+    [SerializeField] private AnimationCurve fallSpeedAnimationCurve;
+
+    public AnimationCurve JumpAnimationCurve => jumpAnimationCurve;
+
+    public float JumpHeight => jumpHeight;
+    public float JumpTime => jumpTime;
+
+    public float FallSpeed => fallSpeed;
+
+    public AnimationCurve FallSpeedAnimationCurve => fallSpeedAnimationCurve;
+}
