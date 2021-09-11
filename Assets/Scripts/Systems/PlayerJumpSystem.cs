3fe9e381cd7bd0c4331ddc5af748414cbcb507aa
solidsign <49648821+solidsign@users.noreply.github.com>
2021-09-11 22:25:00 +0300

    Extracted methods from run in PlayerJumpSystem.cs

--- Assets/Scripts/Systems/PlayerJumpSystem.cs
+++ Assets/Scripts/Systems/PlayerJumpSystem.cs
@@ -18,28 +18,31 @@ namespace Systems
             foreach (var i in _filter)
             {
                 ref var player = ref _filter.Get1(i);
-                if (player.PlayerState == PlayerState.Jumped)
-                {
-                    player.PlayerState = PlayerState.Jumping;
-                    _jumpTimer = 0f;
-                }
-
-                if (player.PlayerState == PlayerState.Jumping)
-                {
-                    HandleJump(player);
-                }
-
-                if (_jumpTimer >= _config.JumpTime)
-                {
-                    player.PlayerState = PlayerState.Falling;
-                }
+                CheckForNewJump(ref player);
+                HandleJump(ref player);
+                CheckJumpEnd(ref player);
             }
+
         }
-        private void HandleJump(Player player)
+
+        private void CheckForNewJump(ref Player player)
         {
+            if (player.PlayerState != PlayerState.Jumped) return;
+            player.PlayerState = PlayerState.Jumping;
+            _jumpTimer = 0f;
+        }
+
+        private void CheckJumpEnd(ref Player player)
+        {
+            if (!(_jumpTimer >= _config.JumpTime)) return;
+            player.PlayerState = PlayerState.Falling;
+        }
+
+        private void HandleJump(ref Player player)
+        {
+            if (player.PlayerState != PlayerState.Jumping) return;
             _currentHeight = _config.JumpAnimationCurve.Evaluate(_jumpTimer / _config.JumpTime) * _config.JumpHeight;
-            player.Rigidbody2D.MovePosition(player.Rigidbody2D.transform.position +
-                                            Vector3.up * (_currentHeight - _lastHeight));
+            player.Rigidbody2D.MovePosition(player.Transform.position + Vector3.up * (_currentHeight - _lastHeight));
             _lastHeight = _currentHeight;
             _jumpTimer += Time.deltaTime;
         }
