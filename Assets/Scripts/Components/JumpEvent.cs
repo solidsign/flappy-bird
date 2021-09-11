f6bf0300d9b6b1a0afba911b7e424c601f42cdb4
solidsign <49648821+solidsign@users.noreply.github.com>
2021-09-11 23:49:43 +0300

    Added PlayerInitSystem.cs

---
+++ Assets/Scripts/Components/JumpEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Components
+{
+    public struct JumpEvent
+    {
+        public bool JustCalled;
+    }
+}
---
+++ Assets/Scripts/Systems/PlayerInitSystem.cs
@@ -0,0 +1,24 @@
+﻿using Components;
+using Leopotam.Ecs;
+using UnityEngine;
+
+namespace Systems
+{
+    public class PlayerInitSystem : IEcsInitSystem
+    {
+        private EcsWorld _world;
+        public void Init()
+        {
+            var player = _world.NewEntity();
+            var obj = GameObject.FindGameObjectWithTag("Player");
+            player
+                .Replace(new Movable
+                {
+                    Displacement = Vector3.zero,
+                    Transform = obj.transform,
+                    Rigidbody2D = obj.GetComponent<Rigidbody2D>()
+                })
+                .Replace(new Player());
+        }
+    }
+}
