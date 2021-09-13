ea3ebdfa23e3b90bf3ff7b1c3fdb292750e2b4af
solidsign <49648821+solidsign@users.noreply.github.com>
2021-09-13 12:23:24 +0300

    Added Obstacle Prefab and components for it

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public struct Obstacle
+{
+    public Transform WholeObstacle;
+    public Transform UpperPipe;
+    public Transform BottomPipe;
+}
---
+++ Assets/Scripts/UnityComponents/AccessToPipes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class AccessToPipes : MonoBehaviour
+{
+    [SerializeField] private Transform upperPipe;
+    [SerializeField] private Transform bottomPipe;
+
+    public Transform BottomPipe => bottomPipe;
+
+    public Transform UpperPipe => upperPipe;
+}
