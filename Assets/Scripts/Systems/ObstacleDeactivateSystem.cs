3f12ff33d50549d0f63d4dddb59e9b32040556a6
solidsign <49648821+solidsign@users.noreply.github.com>
2021-09-15 16:58:25 +0300

    Splitted ObstacleDeactivateSystem.cs into Deactivate system and Pooling system

--- Assets/Scripts/Systems/ObstacleDeactivateSystem.cs
+++ Assets/Scripts/Systems/ObstacleDeactivateSystem.cs
@@ -1,25 +1,16 @@
-﻿using Leopotam.Ecs;
-using UnityComponents;
-using Components;
+﻿using Components;
+using Leopotam.Ecs;
 
 namespace Systems
 {
     public class ObstacleDeactivateSystem : IEcsRunSystem
     {
-        private EcsFilter<Obstacle>.Exclude<Pooled> _filter;
-        private EcsFilter<Dead> _deadPlayer;
-        private Configuration _config;
+        private EcsFilter<Obstacle, Pooled> _filter;
         public void Run()
         {
-            if (!_deadPlayer.IsEmpty()) return;
             foreach (var i in _filter)
             {
-                var obstacle = _filter.Get1(i);
-                if (obstacle.WholeObstacle.position.x < _config.MINObstaclePosX)
-                {
-                    obstacle.WholeObstacle.gameObject.SetActive(false);
-                    _filter.GetEntity(i).Replace(new Pooled());
-                }
+                _filter.Get1(i).WholeObstacle.gameObject.SetActive(false);
             }
         }
     }
---
+++ Assets/Scripts/Systems/ObstaclePoolingSystem.cs
@@ -0,0 +1,25 @@
+﻿using Leopotam.Ecs;
+using UnityComponents;
+using Components;
+
+namespace Systems
+{
+    public class ObstaclePoolingSystem : IEcsRunSystem
+    {
+        private EcsFilter<Obstacle>.Exclude<Pooled> _filter;
+        private EcsFilter<Dead> _deadPlayer;
+        private Configuration _config;
+        public void Run()
+        {
+            if (!_deadPlayer.IsEmpty()) return;
+            foreach (var i in _filter)
+            {
+                var obstacle = _filter.Get1(i);
+                if (obstacle.WholeObstacle.position.x < _config.MINObstaclePosX)
+                {
+                    _filter.GetEntity(i).Replace(new Pooled());
+                }
+            }
+        }
+    }
+}
